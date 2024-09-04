e6d84392fb4dd32883440af77692774eb817f315
CodexScriba <dev.daniel.garcia@gmail.com>
2024-09-03 22:49:20 -0600

    in progress- paraglide bug

--- app/components/Navbar/DesktopNavbar.tsx
+++ app/components/Navbar/DesktopNavbar.tsx
@@ -1,31 +1,38 @@
-//app/components/Navbar/DesktopNavbar.tsx
-import React from 'react';
-import NavLink from './NavLink';
-import * as m from "@/paraglide/messages"
+// //app/components/Navbar/DesktopNavbar.tsx
+// import React from 'react';
+// import NavLink from './NavLink';
+// import * as m from "@/paraglide/messages"
 
+// const DesktopNavbar: React.FC = () => {
+//   const navItems = [
+//     { href: '/', label: m.navbar.home(), prefetch: true },
+//     { href: '/how-it-works', label: m.navbar.howItWorks(), prefetch: true },
+//     { href: '/pricing', label: m.navbar.pricing(), prefetch: true },
+//     { href: '/book-creation', label: m.navbar.bookCreation(), prefetch: false },
+//     { href: '/library', label: m.navbar.library(), prefetch: false },
+//     { href: '/about-us', label: m.navbar.aboutUs(), prefetch: false },
+//   ];
 
-const DesktopNavbar: React.FC = () => {
-  const navItems = [
-    { href: '/', label: m.navbar.home(), prefetch: true },
-    { href: '/how-it-works', label: m.navbar.howItWorks(), prefetch: true },
-    { href: '/pricing', label: m.navbar.pricing(), prefetch: true },
-    { href: '/book-creation', label: m.navbar.bookCreation(), prefetch: false },
-    { href: '/library', label: m.navbar.library(), prefetch: false },
-    { href: '/about-us', label: m.navbar.aboutUs(), prefetch: false },
-  ];
+//   return (
+//     <div className="hidden space-x-4 md:flex">
+//       {navItems.map((item) => (
+//         <NavLink
+//           key={item.href}
+//           href={item.href}
+//           label={item.label}
+//           prefetch={item.prefetch}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
 
-  return (
-    <div className="hidden space-x-4 md:flex">
-      {navItems.map((item) => (
-        <NavLink 
-          key={item.href} 
-          href={item.href} 
-          label={item.label} 
-          prefetch={item.prefetch}
-        />
-      ))}
-    </div>
-  );
+// export default DesktopNavbar;
+
+import React from "react";
+
+const DesktopNavbar = () => {
+	return <div>DesktopNavbar</div>;
 };
 
 export default DesktopNavbar;
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ import Navbar from "./components/Navbar/Navbar";
 export default function Home() {
 	return (
 		<div className="bg-background">
-			<h1>{m.greeting()}</h1>
-			<p>{m.testMessage()}</p>
-			<h2>{m.testMessage()}</h2>
+			<p>{m.test()}</p>{" "}
 		</div>
 	);
 }
