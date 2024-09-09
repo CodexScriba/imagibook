bfab4b87290ea82e375aaf44445f241b5a89e7da
CodexScriba <dev.daniel.garcia@gmail.com>
2024-09-09 00:25:43 -0600

    first commit book-creation

---
+++ app/book-creation/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const bookCreation = () => {
+  return (
+    <div>
+        <Navbar/>
+        <BookCreationForm/>
+    </div>
+  )
+}
+
+export default bookCreation
