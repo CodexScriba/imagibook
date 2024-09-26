de76ca10c5f10f489e4186a3e41a0c4315a8b3a8
daniel0123456789 <daniel0123456789@gmail.com>
2024-09-26 18:29:31 +0000

    Updated schemas

--- app/context/schemas.ts
+++ app/context/schemas.ts
@@ -1,31 +1,42 @@
 // schemas.ts
-import * as z from "zod";
-import * as m from "@/paraglide/messages";
 
-// Step 1 schema (characters)
+import * as z from "zod";
+import * m from "@/paraglide/messages";
+
+//interfaces
+//step1
+export interface Step1Values {
+	characters: {
+		name: string;
+		description?:string;
+	}[];
+}
+//Step2
+export interface Step2Values {
+	mode: "magicWand" | "storybookStudio"
+};
+
+//Step 1 schema (characters)
 export const step1Schema = z.object({
 	characters: z
-		.array(
-			z.object({
-				name: z
-					.string()
-					.min(2, m.characters_errors_nameRequired())
-					.refine((val) => (val.match(/[A-Za-z]/g) || []).length >= 2, {
-						message: m.characters_errors_nameRequired(),
-					}),
-				description: z.string().optional(),
+	  .array(
+		z.object({
+		  name: z
+			.string()
+			.min(2, m.characters_errors_nameRequired())
+			.refine((val) => (val.match(/[A-Za-z]/g) || []).length >= 2, {
+			  message: m.characters_errors_nameRequired(),
 			}),
-		)
-		.min(1, m.characters_errors_atLeastOne()),
-});
+		  description: z.string().optional(),
+		})
+	  )
+	  .min(1, m.characters_errors_atLeastOne()),
+  });
 
-// Step 2 schema (creation mode)
+  // Step 2 schema (creation mode)
 export const step2Schema = z.object({
 	mode: z.enum(["magicWand", "storybookStudio"], {
-		errorMap: () => ({ message: m.creationMode_errors_required() }),
+	  errorMap: () => ({ message: m.creationMode_errors_required() }),
 	}),
-});
-
-// Type definitions for each step
-export type Step1Values = z.infer<typeof step1Schema>;
-export type Step2Values = z.infer<typeof step2Schema>;
+  });
+  
